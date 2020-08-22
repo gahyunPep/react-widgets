2f1cd72e69b276ec5c45227942db41705a0d5277
Gayhun <leeg23@student.douglascollege.ca>
2020-08-21 22:21:31 -0700

    Dropdown box, event bubbling, hook-useRef

---
+++ src/components/Dropdown.js
@@ -0,0 +1,64 @@
+import React, { useState, useEffect, useRef } from 'react';
+
+const Dropdown = ({ options, selected, onSelectedChange }) => {
+  const [open, setOpen] = useState(false);
+  const ref = useRef();
+
+  useEffect(() => {
+    const onBodyClick = (event) => {
+      // if user clicked inside of component don't do anything
+      // when the component is now rendered ref.current is set to null but this event will still invoke when user clicks somewhere in body -> err
+      if(ref.current.contains(event.target)){
+        return;
+      }
+      setOpen(false);
+    }
+
+    document.body.addEventListener('click', onBodyClick);
+
+    //Clean up function will be also invoked when the component is about to remove the component entirely
+    return () => {
+      // remove the eventListener from the body
+      document.body.removeEventListener('click', onBodyClick);
+    };
+  }, []);
+
+  const renderedOptions = options.map((option) => {
+
+    if(option.value === selected.value) {
+      return null; // null in react means render nothing
+    }
+
+    return(
+      <div 
+      key={option.value} 
+      className="item"
+      onClick={()=>{onSelectedChange(option)}}
+      >
+        {option.label}
+      </div>
+    );
+  });
+
+  // console.log(ref.current);
+
+  return (
+    <div ref={ref} className="ui form">
+      <div className="field">
+        <label className="label">Select a Color</label>
+        <div 
+          onClick={()=>setOpen(!open)} 
+          className={`ui selection dropdown ${open? 'visible active': ''}`}
+        >
+          <i className="dropdown icon"></i>
+          <div className="text">{selected.label}</div>
+          <div className={`menu ${open? 'visible transition':''}`}>
+            {renderedOptions}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Dropdown;
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,29 +1,91 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 const Search = () => {
-  const [term, setTerm] = useState('');
+  const [term, setTerm] = useState("programming");
+  const [results, setResults] = useState([]);
 
-  console.log('I RUN WITH EVERY RENDER');
-
+  // cannot pass async function directly
   useEffect(() => {
-    console.log("I RUN WHEN TERM CHANGES");
+    // Workaround 1) en.wikipedia.org/w/api.php?acction=query&list=search&format=json&origin=*&srsearch=
+    const search = async () => {
+      const { data } = await axios.get("https://en.wikipedia.org/w/api.php", {
+        params: {
+          action: "query",
+          list: "search",
+          origin: "*",
+          format: "json",
+          srsearch: term,
+        },
+      });
+      setResults(data.query.search);
+    };
+
+    if (term && !results.length) {
+      search();
+    } else {
+      // setTime out will return timer id
+      const timeoutId = setTimeout(() => {
+        if (term) {
+          search();
+        }
+      }, 500);
+
+      // you can return a  function from useEffect for cleanup purpose
+      return () => {
+        clearTimeout(timeoutId);
+      };
+    }
+
+
+
+    // Workaround 2)
+    // (async () => {
+    //   await axios.get('');
+    // })();
+
+    // Workaround 3) - use promises
+    // axios.get('')
+    //     .then((response) => {
+    //       console.log(response.data);
+    //     });
   }, [term]);
 
+  const renderedResults = results.map((result) => {
+    return (
+      <div className="item" key={result.pageid}>
+        <div className="right floated content">
+          <a
+            className="ui button"
+            href={`https://en.wikipedia.org?curid=${result.pageid}`}
+          >
+            Go
+          </a>
+        </div>
+        <div className="content">
+          <div className="header">{result.title}</div>
+          {/* how to process html in text */}
+          <span dangerouslySetInnerHTML={{ __html: result.snippet }}></span>
+        </div>
+      </div>
+    );
+  });
 
   return (
     <div>
       <div className="ui form">
         <div className="field">
           <label>Enter Search Term</label>
-          <input 
-            value={term} 
-            onChange={e => setTerm(e.target.value)}
+          <input
+            value={term}
+            onChange={(e) => setTerm(e.target.value)}
             className="input"
           />
         </div>
       </div>
+      <div className="ui celled list">{renderedResults}</div>
     </div>
   );
-}
+};
 
 export default Search;
