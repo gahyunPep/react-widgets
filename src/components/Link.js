aa210c59516695c6daeaa69c89ee5bd36e156d6b
amber.lee <amber.lee@phsa.ca>
2020-09-03 18:10:29 -0700

    updating the Route and handling command clicks

--- src/components/Link.js
+++ src/components/Link.js
@@ -1,12 +1,18 @@
 import React from 'react';
 
 const Link = ({ href, className, children }) => {
+    // this change the url, but do not refresh the page
     const onClick = (event) => {
+        // when user press cmd or ctrl, open in a new tab
+        if(event.metaKey || event.ctrlKey) {
+            return;
+        }
+
         event.preventDefault();
         window.history.pushState({}, '', href);
         // tell route component that url changed
         const navEvent = new PopStateEvent('popstate');
-        window.dispatchEve
+        window.dispatchEvent(navEvent);
     }
 
     return <a onClick={onClick} className={className} href={href}>{children}</a>;
